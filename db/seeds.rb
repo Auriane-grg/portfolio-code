2e1e3b9e13791464beba8b95216249335c0747ec
auriane gregoire <auriane.gregoire@gmail.com>
2021-04-07 11:22:32 +0200

    seed comment and animation page projets on mobile

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,5 @@
 Projet.destroy_all 
 
 
-# Projet.create!(name:'Adventure planner', 
-# date:'Mars 2020', 
-# description:'Retrouvez des itinéraires de randonnées pour une escapade en ski de fond. Découvrez votre itinéraire sur une carte 3D, et planifiez votre excurssion pour découvrir le matériel nécessaire, la méteo prévue le jour J... Et pour partir en toute sécurité, partagez votre itinéraire avec d\'autres randonneurs. Vous pouvez même prévoir votre voyage avec eux grâce au chat !', 
-# url:'http://www.adventureplanner.org/', 
-# github: 'https://github.com/Mareenbck/ADVENTUREPLANNER', 
-# host:'Deployed on Heroku', 
-# stack:['ruby on rails', 'javascript', 'css', 'html'])
-
-# Projet.create!(name:'Cocktail app', 
-# date:'Mars 2020', 
-# description:'Cocktail app vous permet de découvrir de nouveaux cocktails, et de partager les vôtres avec vos proches ! Ajoutez-y les recettes de vos cocktails préférés...', 
-# url:'https://cocktail-code.herokuapp.com/cocktails', 
-# github: 'https://github.com/Auriane-grg/cocktail-code', 
-# host:'Heroku', 
-# stack:['ruby on rails', 'javascript', 'css', 'html'])
+# Découvrez des cocktails savoureux et reproduisez leur recette pour 1, 2... autant de personnes que souhaitées ! Ajoutez également vos recettes personnelles pour les partager au plus grand nombre.
+# Planifiez votre excursion en ski de fond en toute sécurité ! Découvrez la carte 3D de votre itinéraire, ses points forts, les équipements nécessaires, la météo prévue pour le jour J, ainsi que des témoignages ! Et pour ne pas partir seul, échangez avec les randonneurs ayant prévu l'excursion le même jour que vous sur le chat.
