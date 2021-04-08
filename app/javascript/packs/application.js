542280285bce124ad529f288f573f3ca2440ca13
auriane gregoire <auriane.gregoire@gmail.com>
2021-04-08 19:06:43 +0200

    link to div projet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,6 +26,7 @@ require("channels")
 import "bootstrap";
 
 import { initNavbar } from '../plugins/init_Navbar';
+import { initScroll} from '../plugins/init_Scroll';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -34,4 +35,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initNavbar();
+  initScroll();
 });
